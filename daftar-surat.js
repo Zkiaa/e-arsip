d484835dfe8de4ed3f1ddf2ed235b4135ee9c3c7
Zakkia <126075084+Zkiaa@users.noreply.github.com>
2023-08-27 18:11:35 +0700

    daftar-surat.js

--- daftar-surat.js
+++ daftar-surat.js
@@ -4,6 +4,7 @@ const formAdd = document.getElementsByClassName("form-add");
 const delBtn = document.getElementsByClassName("del-btn");
 const formDel = document.getElementsByClassName("form-del");
 const cancelBtn = document.getElementById("cancel");
+const cancelBtn1 = document.getElementById("cancel1");
 
 
 addBtn[0].addEventListener("click", () => {
@@ -15,6 +16,6 @@ cancelBtn.addEventListener("click", () => {
 delBtn[0].addEventListener("click", () => {
   formDel[0].classList.add("active");
 });
-cancelBtn.addEventListener("click", () => {
-  formAdd[0].classList.remove("active");
+cancelBtn1.addEventListener("click", () => {
+  formDel[0].classList.remove("active");
 });
