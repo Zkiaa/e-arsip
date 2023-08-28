eced2af9cdf758ac63f72b754308cb0e04f49ef5
Zakkia <126075084+Zkiaa@users.noreply.github.com>
2023-08-29 05:14:53 +0700

    daftar-surat.js

--- daftar-surat.js
+++ daftar-surat.js
@@ -1,21 +1,35 @@
 // side-bar button
-const addBtn = document.getElementsByClassName("add-btn");
-const formAdd = document.getElementsByClassName("form-add");
-const delBtn = document.getElementsByClassName("del-btn");
-const formDel = document.getElementsByClassName("form-del");
-const cancelBtn = document.getElementById("cancel");
-const cancelBtn1 = document.getElementById("cancel1");
+const addBtn = document.querySelector(".add-btn");
+const formAdd = document.querySelector(".form-add");
+const delBtn = document.querySelector(".del-btn");
+const formDel = document.querySelector(".form-del");
+const cancelBtn = document.querySelector("#cancel");
+const cancelBtn1 = document.querySelector("#cancel1");
 
+// Fungsi untuk menonaktifkan semua form
+function disableAllForms() {
+  formAdd.classList.remove("active");
+  formDel.classList.remove("active");
+}
 
-addBtn[0].addEventListener("click", () => {
-  formAdd[0].classList.add("active");
+// Ketika add-btn (tambah arsip) di klik
+addBtn.addEventListener("click", () => {
+  disableAllForms(); // Menonaktifkan semua form
+  formAdd.classList.add("active"); // Mengaktifkan formAdd
 });
+
+// Ketika cancel button ditekan, nonaktifkan formAdd
 cancelBtn.addEventListener("click", () => {
-  formAdd[0].classList.remove("active");
+  formAdd.classList.remove("active");
 });
-delBtn[0].addEventListener("click", () => {
-  formDel[0].classList.add("active");
+
+// Ketika del-btn (hapus arsip) di klik
+delBtn.addEventListener("click", () => {
+  disableAllForms(); // Menonaktifkan semua form
+  formDel.classList.add("active"); // Mengaktifkan formDel
 });
+
+// Ketika cancel1 button ditekan, nonaktifkan formDel
 cancelBtn1.addEventListener("click", () => {
-  formDel[0].classList.remove("active");
+  formDel.classList.remove("active");
 });
